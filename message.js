46cf4f6833fc5507af6f30fac09f735b17092f7f
Navajeevan-99 <saravanannavajeevan99@gmail.com>
2025-01-22 11:15:28 +0530

    Add files via upload

---
+++ message.js
@@ -0,0 +1,4 @@
+function set()
+{
+    document.write("Test");
+}
---
+++ message1.js
@@ -0,0 +1,13 @@
+function set()
+{
+    let x=10
+    x=20
+    var a;
+    document.write(x)
+    document.write(a);
+}
+function set1()
+{
+    let x={Name:"Navajeevan",Rollno:"23ITR109"};
+    document.write(5==5);
+}
